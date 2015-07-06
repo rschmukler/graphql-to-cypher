2be224b47cf51fae370cb91b78f9225ffcd9ed87
Ryan Schmukler <rschmukler@gmail.com>
2015-07-06 12:22:34 -0400

    build setup

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+require('babel/register');
+
+var gulp = require('gulp');
+
+var cache = require('gulp-cached');
+var remember = require('gulp-remember');
+var babel = require('gulp-babel');
+var plumb = require('gulp-plumber');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', ['build'], function() {
+  return gulp.src('test/**/*.js')
+    .pipe(mocha());
+});
+
+gulp.task('build', function() {
+  gulp.src(['src/**/*.js'])
+    .pipe(plumber())
+    .pipe(cache('js'))
+    .pipe(babel())
+    .pipe(remember('js'))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', ['build'], function() {
+  var jsWatch = gulp.watch('src/**/*.js', ['build'])
+    .on('change', checkRemoved('js'));
+
+ function checkRemoved(name) {
+   return function(event) {
+     if (event.type == 'deleted' && cache.caches[name][event.path]) {
+       delete cache.caches[name][event.path];
+       remember.forget(name, event.path);
+     }
+   };
+ }
+});
+
+function plumber() {
+  return plumb({
+    errorHandler: function(err) {
+      console.log(err);
+      this.emit('end');
+    }
+  });
+}
